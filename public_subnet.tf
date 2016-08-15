bbaa3f2fc10aac4042cfecef7469cf1ebf182659
davidski <davidski@deadheaven.com>
2016-08-15 16:15:04 -0700

    Rework terraform support into modules

---
+++ public_subnet.tf
@@ -0,0 +1,9 @@
+resource "aws_subnet" "s1" {
+  vpc_id = "${var.myvpc_id}"
+  availability_zone = "us-west-2c"
+  cidr_block = "172.31.0.0/20"
+}
+
+output "subnet_id" {
+  value = "${aws_subnet.s1.id}"
+}
---
+++ tfvars.tf
@@ -0,0 +1,1 @@
+variable "myvpc_id" {}
