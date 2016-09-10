1dab31d93cff86fe6b796503997c4c1f5383f790
David F. Severski <davidski@deadheaven.com>
2016-09-10 14:58:10 -0700

    Rework public_subnet to autocalculate the CIDR block to use based on VPC range.

--- inputs.tf
+++ inputs.tf
@@ -1,3 +1,5 @@
-variable "myvpc_id" {}
+variable "vpc_id" {}
+
+variable "subnet_cidr" {}
 
 variable "availability_zone" {}
--- public_subnet.tf
+++ public_subnet.tf
@@ -1,9 +1,9 @@
 resource "aws_subnet" "s1" {
-  vpc_id = "${var.myvpc_id}"
+  vpc_id = "${var.vpc_id}"
   availability_zone = "${var.availability_zone}"
-  cidr_block = "172.31.0.0/20"
-}
-
-output "subnet_id" {
-  value = "${aws_subnet.s1.id}"
+  cidr_block = "${var.subnet_cidr}",
+  tags {
+    description = "Allows direct inbound access"
+    Name = "Public Subnet"
+  }
 }
