348292de7858790d65a657fd9660903429f1cd43
David F. Severski <davidski@deadheaven.com>
2016-12-08 13:58:40 -0800

    Ensure public subnets have the automap public IP flag set to true

--- public_subnet.tf
+++ public_subnet.tf
@@ -1,10 +1,12 @@
 resource "aws_subnet" "s1" {
-  vpc_id = "${var.vpc_id}"
-  availability_zone = "${var.availability_zone}"
-  cidr_block = "${var.subnet_cidr}",
+  vpc_id                  = "${var.vpc_id}"
+  availability_zone       = "${var.availability_zone}"
+  map_public_ip_on_launch = true
+  cidr_block              = "${var.subnet_cidr}"
+
   tags {
     description = "Allows direct inbound access"
-    Name = "Public Subnet"
-    managed_by = "Terraform"
+    Name        = "Public Subnet"
+    managed_by  = "Terraform"
   }
 }
